6c59e1ad332dd6eab84c9b2200f152e15e27bbed
1147279 <1147279@students.wits.ac.za>
2018-04-09 23:29:15 +0200

    With timer and output file

--- DarrenPython.py
+++ DarrenPython.py
@@ -1,9 +1,9 @@
 from mrjob.job import MRJob
 import os
-
+import time
 
 class matrixMultiply(MRJob):
-
+	
 	def sortbyJ(listItem):
 		return listItem[0]
 	
@@ -12,7 +12,7 @@ class matrixMultiply(MRJob):
 		#info2 = matrixN.readline()
 		if len(line.split()) == 3: 
 			nameFile = os.environ['map_input_file']
-			if nameFile == "test.txt":			
+			if nameFile == "outA1.list":			
 				iM,jM,valueM =  line.split()
 				#print '---',iM,jM,valueM,jN,kN,valueN,'---'
 				for k in range(0,int(columnsN)):
@@ -28,32 +28,42 @@ class matrixMultiply(MRJob):
 	def reducer(self, key, values):
 		oldlistM = []
 		oldlistN = []
+		
 		for i in values:
 			if i[0]=='M':
 				oldlistM.append([i[1],i[2]])
 			else:
 				oldlistN.append([i[1],i[2]])
-		print oldlistM
-		print oldlistN
+		#print oldlistM
+		#print oldlistN
 		listM = sorted(oldlistM, key=lambda x:x[1])
 		listN = sorted(oldlistN, key=lambda x:x[1])
-		print listM
-		print listN
+		#print listM
+		#print listN
 		P = []
 		k=0;
 		#for i in range((len(listM)/(int(columnsM))-1)*int(columnsM),len(listM)):
 		for i in listM:
 			for l in range(0,len(listN)):
 				if listN[l][0]==i[0]:			
-					P.append(i[1]*listN[k][1])
+					P.append(i[1]*listN[l][1])
 					k=k+1
-					print P
 				else:
 					P.append(0)
+					
 		#key.sort()
 		sumofP=sum(P)
 		if sumofP != 0:
 			yield key,sumofP
+			f=""
+			g=""
+			keystr=str(key).split(',')
+			for i in range(1,len(keystr[0])):
+				f+=keystr[0][i]
+			for i in range(0,len(keystr[1])-1):
+				g+=keystr[1][i]
+			stringOut = f+' '+g+' '+str(sumofP)+"\n"
+			outputFile.write(stringOut)
 		
 
 		#print key, sum(P)
@@ -61,13 +71,21 @@ class matrixMultiply(MRJob):
 
 
 if __name__ == '__main__':
-	matrixM = open("test.txt","r")
+	matrixM = open("outA1.list","r")
 	rowsM,columnsM = matrixM.readline().split()
-	matrixN = open("test2.txt","r")
-	rowsN,columnsN = matrixN.readline().split()	
-			
-	#P = [] #Answer Matrix    
+	matrixN = open("outB1.list","r")
+	rowsN,columnsN = matrixN.readline().split()
+	print "columnsN=", columnsN
+	outputFile = open('Output.txt','w')
+	outputFile.write(str(rowsM)+' '+str(columnsN)+"\n")	
+	starttime = time.time()  
 	matrixMultiply.run()
+	endtime = time.time()
+	duration = endtime-starttime
+	print "Time: ", duration 
+	outputFile.close()
+	matrixM.close()
+	matrixN.close()
 	
 
 
